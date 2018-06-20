fda66f8ababd40a73eb0dcab411c427e4e51ce1b
Marek Ostrowski <mostrowski@transperfect.com>
2018-06-20 11:10:15 +0200

    working on modifying graphic component

--- src/theme/adapt-contrib-vanilla/js/vanilla.js
+++ src/theme/adapt-contrib-vanilla/js/vanilla.js
@@ -6,7 +6,15 @@ define(function(require) {
 
 	// Block View
 	// ==========
-
+Adapt.on('pageView:ready', function () {
+        $('.paglogotop').css('opacity', 0).delay(800).animate({
+            opacity: 1
+        });
+    });
+    
+    
+    
+    
 	Adapt.on('blockView:postRender', function(view) {
 		var theme = view.model.get('_theme');
 		
